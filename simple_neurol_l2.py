5bf78075500e4585381c1efac9d2d3ff2917a974
stasonhan <huade20@126.com>
2018-01-31 05:36:50 -0800

    modify a little to simple_neurol_l2.py

--- simple_neurol_l2.py
+++ simple_neurol_l2.py
@@ -2,24 +2,27 @@ import numpy as np
 from numpy.random import RandomState
 import tensorflow as tf
 
-batch_size = 8
 
 def get_weight(shape,labd):
     var = tf.Variable(tf.random_normal(shape),dtype=tf.float32)
     tf.add_to_collection('losses',tf.contrib.layers.l2_regularizer(labd)(var))
     return var
 
-W1 = tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
+#W1 = tf.Variable(tf.random_normal([2,3],stddev=1,seed=1))
 #W2 = tf.Variable(tf.random_normal([3,1],stddev=1,seed=1))
 x = tf.placeholder(tf.float32,shape=(None,2),name="x-input")
 y_ = tf.placeholder(tf.float32,shape=(None,1),name="y-input")
+batch_size = 8
 
-#a = tf.matmul(x,W1)
 y = tf.matmul(x,W1)
 
-loss_less = 10
-loss_more = 1
+layer_dimension = [2,10,10,10,1]
+n_layers = len(layer_dimension)
 
+cur_layer = x
+
+in_dimension=layer_dimension[0]
+for 
 loss = tf.reduce_sum(tf.where(tf.greater(y,y_),(y-y_)*loss_more,(y_-y)*loss_less))
 
 #cross_entropy = -tf.reduce_mean(y_ * tf.log(tf.clip_by_value(y,1e-10,1.0)))
