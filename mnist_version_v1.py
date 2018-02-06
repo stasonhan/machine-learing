aaa80b8a726ba30399bc63ae36d0f2d8743fd6db
hanhuade <huade.han@hermes-sys.com>
2018-02-06 01:28:37 -0800

    modified:   mnist_version_v1.py

--- mnist_version_v1.py
+++ mnist_version_v1.py
@@ -1,11 +1,13 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
+#-*-coding : utf-8-*-
+
 import tensorflow as tf
 
 
 from tensorflow.examples.tutorials.mnist import input_data
 
 INPUT_NODE = 784
-OUT_PUT = 10
+OUTPUT_NODE = 10
 LAYEER1_NODE = 500
 
 BATCH_SIZE = 100
@@ -20,9 +22,9 @@ MOVING_AVERAGE_DECAY = 0.99
 
 def inference(input_tensor,avg_class,weights1,biases1,weights2,biases2):
     # if there is no ExponentialMovingAverage
-    if avg_class == None：
-        #计算隐层的前向传播
-        layer1 = tf.nn.relu(tf.matmul(iput_tensor,weights1) + biases1)
+    if avg_class == None:
+      
+        layer1 = tf.nn.relu(tf.matmul(input_tensor,weights1) + biases1)
         return tf.matmul(layer1,weights2) + biases2
     else:
         layer1 = tf.nn.relu(tf.matmul(input_tensor,avg_class.average(weights1)) + avg_class.average(biases1))
@@ -31,10 +33,10 @@ def inference(input_tensor,avg_class,weights1,biases1,weights2,biases2):
 def train(mnist):
     x = tf.placeholder(tf.float32,[None,INPUT_NODE],name='x-input')
     y_= tf.placeholder(tf.float32,[None,OUTPUT_NODE],name='y-input')
-    weights1 = tf.Variable(tf.truncate_normal([INPUT_NODE,LAYEER1_NODE],stddev=0.1))
+    weights1 = tf.Variable(tf.truncated_normal([INPUT_NODE,LAYEER1_NODE],stddev=0.1))
     biases1 = tf.Variable(tf.constant(0.1,shape=[LAYEER1_NODE]))
     
-    weights2 = tf.Variable(tf.truncate_normal([LAYEER1_NODE,OUTPUT_NODE],stddev=0.1))
+    weights2 = tf.Variable(tf.truncated_normal([LAYEER1_NODE,OUTPUT_NODE],stddev=0.1))
     biases2 = tf.Variable(tf.constant(0.1,shape=[OUTPUT_NODE]))
     
     y = inference(x,None,weights1,biases1,weights2,biases2)
@@ -42,4 +44,50 @@ def train(mnist):
     global_step = tf.Variable(0,trainable=False)
     
     variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY,global_step)
+    # tf.trainable_variables 
     variable_averages_op = variable_averages.apply(tf.trainable_variables())
+    # 
+    average_y = inference(x,variable_averages,weights1,biases1,weights2,biases2)
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y,logits=tf.argmax(y_,1))
+    
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
+    
+    regularizer = tf.contrib.layers.l2_reglularizer(REGULARIZATION_RATE)
+    regularization = regularizer(weights1) + regularizer(weights2)
+    loss = cross_entropy_mean + regularization
+    
+    learning_rate = tf.train.exponential_decay(
+                            LEARNING_RATE_BASE, 
+                            global_step,mnist.train.num_examples,
+                            LEARNING_RATE_DECAY)
+    
+    train_step = tf.train.GradientDescentOptimizer(learning_rate)\
+                   .minimize(loss,global_step=global_step)
+    with tf.control_dependencies([train_step,variable_averages_op]):
+        train_op = tf.no_op(name="train") #
+    correct_prediction = tf.equal(tf.argmax(average_y,1),tf.argmax(y_,1))
+    
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+    
+    with tf.Session() as sess:
+        tf.initialize_all_variables().run()
+        validate_feed = {x :mnist.validation.images,
+                         y_: mnist.validation.lables}
+        test_feed = {x: mnist.test.images,y_:minst.test.lables}
+        
+        for i in range(TRAINING_STEPS):
+            if i % 1000 == 0:
+                validate_acc = sess.run(accuracy,feed_dict=validate_feed)
+                print ("After %d training step(s), vlaidation accuracy"
+                       "using average model is %g" % (i,validate_acc))
+        test_acc = sess.run(accuracy,feed_dict=test_feed)
+        print ("After %d training step(s), vlaidation accuracy"
+               "using average model is %g" % (TRAINING_STEPS,test_acc))
+def main(argv = None):
+    mnist = input_data.read_data_sets("/home/dev/code/mnist",one_hot = True)
+    train(mnist)
+
+if __name__ == '__main__':
+    tf.app.run()
+         
+        
