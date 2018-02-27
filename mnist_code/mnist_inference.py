7f13311652ffc067ff7623e550f839f11d92aeaa
hanhuade <huade.han@hermes-sys.com>
2018-02-26 21:47:34 -0800

    mnist_code

--- mnist_code/mnist_inference.py
+++ mnist_code/mnist_inference.py
@@ -9,26 +9,26 @@ LAYEER1_NODE = 500
 def get_weight_variable(shape,regularizer):
     weights = tf.get_variable(
 	"weights",shape,
-	initializer=tf.truncate_normal_initializer(stddev=0.1))
+	initializer=tf.truncated_normal_initializer(stddev=0.1))
 	
-	if regularizer != None:
-	    tf.add_to_collection('losses',regularizer(weights))
-	return weights
+    if regularizer != None:
+        tf.add_to_collection('losses',regularizer(weights))
+    return weights
 	
 def inference(input_tensor,regularizer):
-    with tf.variables_scope('layer1'):
-	    weights=get_weight_variable(
+    with tf.variable_scope('layer1'):
+        weights=get_weight_variable(
 		        [INPUT_NODE,LAYEER1_NODE],regularizer)
-		biases = tf.get_variable("biases",[LAYEER1_NODE],
-		                        initializer=tf.constant_initilizer(0.0))
-		layer1= tf.nn.relu(tf.matmul(input_tensor,weights)+biases)
+        biases = tf.get_variable("biases",[LAYEER1_NODE],
+		                        initializer=tf.constant_initializer(0.0)) 
+        layer1= tf.nn.relu(tf.matmul(input_tensor,weights)+biases)
 		
-	with tf.variables_scope('layer2'):
-	    weights = get_weight_variable(
+    with tf.variable_scope('layer2'):
+        weights = get_weight_variable(
 		        [LAYEER1_NODE,OUTPUT_NODE],regularizer)
-		biases = tf.get_variable(
+        biases = tf.get_variable(
 		            "biases",[OUTPUT_NODE],
-					initializer=tf.constant_initilizer(0.0))
-		layer2 = tf.matmul(layer1,weights) + biases
-	return layer2
+					initializer=tf.constant_initializer(0.0))
+        layer2 = tf.matmul(layer1,weights) + biases
+    return layer2
 	
