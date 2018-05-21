93e145c979b1b0d3bc628dbedb1a84dd7b2237aa
stasonhan <huade.han@eyou.com>
2018-05-21 07:17:31 -0700

    debug remove some file

---
+++ openai/debug.py
@@ -0,0 +1,8 @@
+import numpy as np
+all_gradients=np.load('all_gradients.npy').tolist()
+all_rewards = np.load('all_rewards.npy').tolist()
+
+
+for var_index in range(4):
+    a=[reward * all_gradients[game_index][step][var_index] for game_index,rewards in enumerate(all_rewards) for step,reward in enumerate(rewards)]
+    
