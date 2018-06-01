612cdbb3cd966fc0342a738a16c27ac75dbcdb57
hanhuade <huade.han@hermes-sys.com>
2018-06-01 02:46:55 -0700

    planemunk.py

--- self-plane/planemunk.py
+++ self-plane/planemunk.py
@@ -11,6 +11,8 @@ from pygame.color import THECOLORS
 from pymunk.vec2d import Vec2d
 from pymunk.pygame_util import draw
 import time
+from turtledemo import planet_and_moon
+from _codecs import charmap_build
 # The size of the screen to display
 WIDTH = 1920
 HEIGHT= 1080
@@ -31,7 +33,7 @@ class Demo(object):
         self.crashed = False
         self.space = pymunk.Space()
         self.space.gravity=Vec2d((0.,0.))
-        self.create_plane(100, 100, 0.5)
+        self.create_plane(0, 0, 0.5)
         
         static = [
             pymunk.Segment(
@@ -54,6 +56,14 @@ class Demo(object):
             s.color = THECOLORS['red']
         self.space.add(static)
         
+    def get_state(self,x,y,angle):
+        """
+        """
+        reading = []
+        arm_left = self.make_sensor_arm(x,y)
+        arm_middle = arm_left
+        arm_right = arm_left
+        reading.append()
     def create_plane(self,x,y,r):
         """
         x,y positon ,r the angle
@@ -70,21 +80,31 @@ class Demo(object):
         self.space.add(self.plane_body,self.plane_shape)
     
     def set_target(self,x,y,r):
-        target_body = pymunk.Body(pymunk.inf,pymunk.inf)
-        target_shape = pymunk.Circle(target_body,r)
-        target_shape.elasticity = 1.0
-        target_body.position = x,y
-        target_shape.color = THECOLORS['blue']
-        self.space.add(target_body,target_shape)
-        return target_body
-            
+        self.target_body = pymunk.Body(pymunk.inf,pymunk.inf)
+        self.target_shape = pymunk.Circle(self.target_body,r)
+        self.target_shape.elasticity = 1.0
+        self.target_body.position = x,y
+        self.target_body.angle = Vec2d(x,y).get_angle()
+        self.target_shape.color = THECOLORS['blue']
+        self.space.add(self.target_body,self.target_shape)
+                    
     def frame_step(self,action):
         if action == 0:
             self.plane_body.angle -= .2
-        if action == 1:
+        elif action == 1:
             self.plane_body.angle += .2
+        #else:
+        #    self.plane_body.angle = Vec2d()
+        #flying_direction = Vec2d(1,0).rotated(self.plane_body.angle)
+        planet_position_vector = Vec2d(self.plane_body.position).normalized()
+        target_pisition_vector = Vec2d(self.target_body.position).normalized()
         
-        flying_direction = Vec2d(1,0).rotated(self.plane_body.angle)
+        angle = target_pisition_vector.get_angle_between(target_pisition_vector)
+        
+        #flying_direction = Vec2d(1,0).rotated(self.target_body.angle)
+        flying_direction = target_pisition_vector.rotated(angle)
+        self.plane_body.angle = flying_direction.get_angle()
+        print (self.plane_body.angle)
         self.plane_body.velocity = 100 * flying_direction
         
         screen.fill(THECOLORS["black"])
@@ -96,7 +116,8 @@ class Demo(object):
 
 if __name__ == "__main__":
     plane = Demo()
-    plane.set_target(1900, 600,50)
+    plane.set_target(1900, 1060,50)
     while True:
-        plane.frame_step(random.randint(0,2))
+        #plane.frame_step(random.randint(0,2))
+        plane.frame_step(2)
         time.sleep(0.05)
